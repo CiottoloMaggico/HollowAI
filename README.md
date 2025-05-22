2be0c06caf30e12fb9e99d544fae75c8ea5460be
CiottoloMaggico <fantozz2003@outlook.com>
2025-05-22 16:10:59 +0200

    readme and easy config.yaml

--- README.md
+++ README.md
@@ -1,10 +1,79 @@
 # HollowAI
-A RL agent that beats Hollow Knight bosses
+This repo implements a reinforcement learning agent that (hopefully) beats bosses in _Hollow Knight_.
+## 🧠 How it works:
+HollowAi is actually an interface between: 
+- **sb3 agents** 
+- **HollowGym**, our custom mod that exposes a Gym-like environment for Hollow Knight\
 
-### Tensorboard Plots
+HollowGym handles the game logic: controlling the character, gathering observations, and computing rewards. Communication between HollowAI and HollowGym happens via WebSocket, using JSON-encoded messages.
+### Communication protocol
+The client (HollowGym) and server (HollowAI) exchange messages through a simple command system. The supported commands are:
+- Exit – Clean shutdown
+- Reset – Reset the environment
+- Step – Perform an action
+- Observation – Request current observation
+- Ready – Handshake to sync server and client
+- Error – Report issues
+
+Here's a basic example of a Step command:
+
+```json5
+// server -> client
+{
+  "Cmd": 2, // Step
+  "Data": {
+    "Action": "0000", // no action
+  },
+}
+```
+```json5
+// response from the client
+{
+  "Cmd": 2,
+  "Data": {
+    "Action": "0000",
+    "Observation": {
+      // observation after the action
+    },
+    "Info": {
+      // some info about the environment and the current episode
+    },
+  }
+}
+```
+#### The Handshake
+Before any interaction can happen, the client and server perform a three-way handshake:
+1. The client sends a Ready message.
+2. The server responds with configuration details (from config.yaml).
+3. The client confirms by returning its supported settings, including the correct observation space for the specified boss.
+
+This ensures both components are in sync before training begins.
+
+_Note: all the information about HollowGym protocol can be found [here](https://github.com/CiottoloMaggico/HKFeeder)_
+
+### Stable baselines 3
+Instead of reinventing the wheel (and doing it poorly), we rely on the excellent Stable Baselines3 library. It provides reliable, state-of-the-art implementations of all the RL algorithms we need.
+
+### Getting started
 ---
-Use `tensorboard --logdir ./logs` to open tensorboard webserver, and go in **Scalars** tab
-- Note: it plots all the data it finds in the log folder (events.out.tfevents* files), these are not deleted after every training so you'll see everything together, consider deleting them or look at the plots with Relative or Wall format
+#### Requirements: 
+- A copy of **Hollow Knight**
+- **HollowGym** installed and working
+- This repo: **HollowAi**
+#### Setup instructions
+1. Create a python environment and install the dependencies.\
+_If your machine have a cuda capable gpu, and you want to use it, install pytorch using the command provided [here](https://pytorch.org/get-started/locally/)._
+2. Edit the 'config.yaml' file to suit your setup preferencies.
+3. Start **HollowAi** by running `python main.py`.
+4. Launch `n_envs` instances of Hollow Knight.
+5. In each instance:
+   1. open a save file 
+   2. once the game is loaded press `F8` to tell HollowAi that the client is ready
+6. **You're all set!**
+
+### Tensorboard Integration
+---
+Use `tensorboard --logdir ./logs` to open tensorboard webserver, and go in **Scalars** tab to see all the stats
 
 ### Reward Function
 ---
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import datetime
 import logging
+import os.path
 import sys
+import yaml
 
 from stable_baselines3 import DQN
 from stable_baselines3.common.callbacks import CheckpointCallback, CallbackList
@@ -10,95 +12,51 @@ from envs.utils import create_env
 from utils.logger import LoggingCallback, EvaluationLogger
 
 # Create log
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[logging.FileHandler("logs/debug.log"), logging.StreamHandler(sys.stdout)],
-)
-
-main_logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=[logging.StreamHandler(sys.stdout)])
 logger = logging.getLogger(__name__)
 
+def load_config():
+    logger.info("Loading config")
+    with open("config.yaml") as conf:
+        conf = yaml.load(conf, Loader=yaml.FullLoader)
+
+    logger.info("Config loaded, validating...")
+    assert(not conf["run"]["load_existing"] or conf["agent"]["load_model"])
+    assert(conf["run"]["action"] == "training" or conf["agent"]["action"] == "evaluation")
+    logger.info("Config successfully loaded and validated")
+
+    return conf["gym"], conf["agent"], conf["callbacks"], conf["training"], conf["evaluation"], conf["run"]
+
 def main():
-    # Edit training parameters here
-    TOTAL_TIMESTEPS = 100000
-    EVALUATION_EPISODES = 4
-    TARGET_FRAMERATE = 100
-    DISABLE_RENDERING = False
-    FRAME_SKIP = 1
-    N_ENVS = 1
-    GAME_SPEED = 1
-    BOSS_NAME = "Hornet Boss 1"
-    SCENE_NAME = "GG_Hornet_1"
-    MODEL_TO_LOAD = "Hornet Boss 1_DQN_2025-05-21 18:15:00.057472_300000_steps.zip"
-    REPLAY_BUFFER_TO_LOAD = "Hornet Boss 1_DQN_2025-05-21 18:15:00.057472_replay_buffer_300000_steps.pkl"
-    DO_TRAINING = False
-    DO_EVAL = True
-    # -----------------------------
+    gym, agent, callbacks, train, eval, run = load_config()
+    model_name = f"{gym['boss_name']}_{run['action']}"
 
-    env = create_env(FRAME_SKIP, GAME_SPEED, BOSS_NAME, SCENE_NAME, n_envs=N_ENVS, disable_rendering=DISABLE_RENDERING,
-                     target_framerate=TARGET_FRAMERATE)
-    model_name = f"{BOSS_NAME}_DQN_{datetime.datetime.now()}" if not MODEL_TO_LOAD else MODEL_TO_LOAD
-
-    if not MODEL_TO_LOAD:
-        model = DQN(
-            "MlpPolicy",
-            env,
-            verbose=1,
-            learning_starts=5000,  # to not reinforce bad guesses due to initial exploration (+ let buffer fill up)
-            learning_rate=3e-5,  # how big each update to the Q-network weights is during training.
-            gamma=0.95,  # discount factor: how much an agent prioritizes future rewards over immediate ones
-            tau=1,  # soft update coeff: how fast the target network moves toward the online network
-            buffer_size=100_000,
-            batch_size=64,
-            train_freq=(4, "step"),
-            gradient_steps=1,  # how many gradient updates per step
-            exploration_initial_eps=.95,  # start exploration rate
-            exploration_final_eps=.05,  # end exploration rate
-            exploration_fraction=0.8, # expl. rate will linearly decrease from start to end in (exploration_fraction * total_timesteps) steps
-            tensorboard_log="./logs/",
-        )
+    env = create_env(**gym)
+    if run["load_existing"]:
+        logger.info("Loading existing model")
+        model = DQN.load(f"./checkpoints/{agent['load_model']}", env=env, **agent["hyperparameters"])
+        if agent["load_replay_buffer"]: model.load_replay_buffer(path=f"./checkpoints/{agent['load_replay_buffer']}")
+        logger.info(f"Successfully loaded existing model")
     else:
-        model = DQN.load(
-            f"./checkpoints/{MODEL_TO_LOAD}",
-            env=env,
-            learning_starts=0,
-            learning_rate=7e-5,
-            exploration_initial_eps=.5,
-            exploration_final_eps=.2,
-            exploration_fraction=1,
-        )
-        if REPLAY_BUFFER_TO_LOAD: model.load_replay_buffer(path=f"./checkpoints/{REPLAY_BUFFER_TO_LOAD}")
+        logger.info("Creating new model")
+        model = DQN("MlpPolicy", env=env, verbose=1, tensorboard_log="./logs/", **agent["hyperparameters"])
+        logger.info(f"Successfully created new model")
 
-    logger.info("Creating model callbacks")
-    checkpoint_callback = CheckpointCallback(
-        save_freq=25_000,
-        save_path="./checkpoints/",
-        name_prefix=model_name,
-        save_replay_buffer=True,
-        save_vecnormalize=True,
-    )
-    logging_callback = LoggingCallback(verbose=1)
-    eval_callback = EvaluationLogger(f"./logs/eval/{model_name}", N_ENVS)
-    env_callback = CallbackList([checkpoint_callback, logging_callback])
-    logger.info("Environment callbacks ready")
+    if run["action"] == "training":
+        logger.info("Creating training callbacks")
+        checkpoint_callback = CheckpointCallback(save_path="./checkpoints/", name_prefix=model_name, **callbacks["checkpoint_callback"])
+        logging_callback = LoggingCallback(verbose=1)
+        env_callback = CallbackList([checkpoint_callback, logging_callback])
+        logger.info("Callbacks ready, starting training")
 
-    if DO_TRAINING:
-        logger.info("Model ready, starting training...")
-        model.learn(
-            total_timesteps=TOTAL_TIMESTEPS,
-            callback=env_callback,
-            tb_log_name=model_name,
-            progress_bar = True,
-        )
+        model.learn(callback=env_callback, tb_log_name=model_name, **train)
+
         logger.info("Model training completed, saving model...")
         model.save(f"./checkpoints/{model_name}")
         logger.info("Model saved")
-
-    if DO_EVAL and EVALUATION_EPISODES > 0:
-        evaluate_policy(
-            model, env, n_eval_episodes=EVALUATION_EPISODES, callback=eval_callback
-        )
+    else:
+        eval_callback = EvaluationLogger(f"./logs/{model_name}", gym["n_envs"])
+        evaluate_policy(model, env, **eval)
 
 if (__name__ == "__main__"):
     main()
